cc2cd7e815d743c27f0d3801df1c7398d53a7973
ein <forein@qq.com>
2020-10-28 00:19:20 +0800

    201028

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,24 @@
-// pages/search/index.js
+import { request } from "../../request/index.js"
+import regeneratorRuntime from "../../lib/runtime/runtime.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    goods: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleInput(e){
+    const {value} = e.detail;
+    if(!value.trim()){
+      this.setData({
+        goods: []
+      })
+      return;
+    }else{
+      this.qseacrh(value);
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  async qseacrh(query){
+    const res = await request({url: "/goods/qsearch", data: {query}});
+    this.setData({
+      goods:res.data.message
+    })
   }
 })
