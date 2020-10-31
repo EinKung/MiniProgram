3bfd80de1dfb525ae464816bdb78bd035d57663c
ein <forein@qq.com>
2020-11-01 00:30:38 +0800

    201101

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,16 +1,63 @@
-// pages/feedback/index.js
+import { showToast } from "../../utils/asyncwx.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    array: ['美国', '中国', '巴西', '日本'],
-    index:0
+    PTypeList: ['使用体验/反馈', '使用体验/优化', '客服投诉'],
+    index:0,
+    chosenPics: [],
+    textVal:""
   },
+  TimeId:-1,
   bindPickerChange(e){
     this.setData({
       index:e.detail.value
     })
+  },
+  handleImgChoose(){
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original','compressed'],
+      sourceType: ['album','camera'],
+      success: (result)=>{
+        this.setData({
+          chosenPics:[...this.data.chosenPics,...result.tempFilePaths]
+        })
+      }
+    });
+  },
+  handleRemovePic(e){
+    const {index} = e.currentTarget.dataset;
+    let {chosenPics} = this.data;
+    chosenPics.splice(index, 1);
+    this.setData({
+      chosenPics
+    })
+  },
+  handleTextInput(e){
+    const {value} = e.detail;
+    this.setData({
+      textVal:value
+    })
+  },
+  handleFormCommit(){
+    const {textVal} = this.data;
+    if(!textVal.trim()){
+      showToast({msg:"IllegeL InpuT!"});
+    }else{
+      this.setData({
+        textVal:"",
+        chosenPics:[]
+      })
+      showToast({msg:"CommiT SuccessfullY"});
+      clearTimeout(this.TimeId);
+      this.TimeId=setTimeout(()=>{
+        wx.navigateBack({
+          delta: 1
+        });
+      }, 1000);
+    }
   }
 })
